6eff1d002b0b6ea6e15beec2d9b3bb972cd0ca9d
AlexNek <alexnek@russinger.com>
2023-08-20 13:47:26 +0200

    update comments

--- src/FileSystemPickResult.cs
+++ src/FileSystemPickResult.cs
@@ -6,11 +6,11 @@
     public class FileSystemPickResult
     {
         /// <summary>
-        /// Initializes a new instance of the FilePickResult class.
+        /// Represents the result of picking a file system item.
         /// </summary>
-        /// <param name="name">The name of the file.</param>
-        /// <param name="fullPath">The full path of the file.</param>
-        /// <param name="platformPath">The platform-specific path of the file.</param>
+        /// <param name="name">The name of the file system item.</param>
+        /// <param name="fullPath">The full path of the file system item.</param>
+        /// <param name="platformPath">The platform specific path of the file system item.</param>
         public FileSystemPickResult(string? name, string? fullPath, string? platformPath)
         {
             Name = name;
@@ -18,12 +18,18 @@
             PlatformPath = platformPath;
         }
 
+        /// <summary>
+        /// Gets the name of a file system item.
+        /// </summary>
         public string? Name { get; }
 
+        /// <summary>
+        /// Gets the full path of the file system item.
+        /// </summary>
         public string? FullPath { get; }
 
         /// <summary>
-        /// Platform path for read or write
+        /// Gets the platform specific path of the file system item
         /// </summary>
         public string? PlatformPath { get; }
     }
