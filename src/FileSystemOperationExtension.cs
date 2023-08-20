b35cf4198428a0c517835e5ee087ae5b704ff304
AlexNek <alexnek@russinger.com>
2023-08-20 12:31:41 +0200

    add DI Extension

---
+++ src/FileSystemOperationExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonDialogs.Maui
+{
+    public static class FileSystemOperationExtension
+    {
+        public static void AddFileSystemOperations(this IServiceCollection services)
+        {
+            services.AddSingleton<IFileSystemOperations, FileSystemOperations>();
+        }
+    }
+}
